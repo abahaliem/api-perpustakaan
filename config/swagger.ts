e06cf189c7633a986a5ad74c4fb5b6ded02fdb26
abah_aliem <alami4238@gmail.com>
2022-12-17 18:18:45 +0700

    update uiurl swagger

--- config/swagger.ts
+++ config/swagger.ts
@@ -4,7 +4,7 @@ import { SwaggerConfig } from '@ioc:Adonis/Addons/Swagger'
 export default {
 
 	uiEnabled: true, //disable or enable swaggerUi route
-	uiUrl: 'docs', // url path to swaggerUI
+	uiUrl: 'https://api-perpustakaan.up.railway.app/docs', // url path to swaggerUI
 	specEnabled: true, //disable or enable swagger.json route
 	specUrl: '/swagger.json',
 	
@@ -21,6 +21,7 @@ export default {
 				description: 'Nodejs Backend Dev With Adonis'
 			},
 			components: {
+				
 				validatorUrl: null,
 				securitySchemes: {
 					bearerAuth: {
